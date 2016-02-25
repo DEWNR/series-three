e79cd81e1671aafec0616e27f2e9756d40fc927e
Ben Kannenberg <bkannenberg@gmail.com>
2016-02-25 14:34:46 +1030

    Remove iconFont

--- gulpfile.js/tasks/html.js
+++ gulpfile.js/tasks/html.js
@@ -10,6 +10,8 @@ var htmlmin      = require('gulp-htmlmin')
 var path         = require('path')
 var render       = require('gulp-nunjucks-render')
 var fs           = require('fs')
+var jsoncombine  = require("gulp-jsoncombine")
+var gulpSequence = require('gulp-sequence')
 
 var exclude = path.normalize('!**/{' + config.tasks.html.excludeFolders.join(',') + '}/**')
 
@@ -20,20 +22,34 @@ var paths = {
 
 var getData = function(file) {
   var dataPath = path.resolve(config.root.src, config.tasks.html.src, config.tasks.html.dataFile)
+
   return JSON.parse(fs.readFileSync(dataPath, 'utf8'))
 }
 
-var htmlTask = function() {
-  render.nunjucks.configure([path.join(config.root.src, config.tasks.html.src)], {watch: false })
 
-  return gulp.src(paths.src)
-    .pipe(data(getData))
-    .on('error', handleErrors)
-    .pipe(render())
-    .on('error', handleErrors)
-    .pipe(gulpif(process.env.NODE_ENV == 'production', htmlmin(config.tasks.html.htmlmin)))
-    .pipe(gulp.dest(paths.dest))
-    .pipe(browserSync.stream())
+var jsonTask = function(cb) {
+    var dataFiles = path.resolve(config.root.src, config.tasks.html.src, 'data/*.json')
+
+    return gulp.src(dataFiles)
+      .pipe(jsoncombine("global.json",function(data){
+          return new Buffer(JSON.stringify(data))
+      }))
+      .pipe(gulp.dest("./src/html/data"));
+}
+
+
+
+var htmlTask = function(cb) {
+    render.nunjucks.configure([path.join(config.root.src, config.tasks.html.src)], {watch: false })
+
+    return gulp.src(paths.src)
+      .pipe(data(getData))
+      .on('error', handleErrors)
+      .pipe(render())
+      .on('error', handleErrors)
+      .pipe(gulpif(process.env.NODE_ENV == 'production', htmlmin(config.tasks.html.htmlmin)))
+      .pipe(gulp.dest(paths.dest))
+      .pipe(browserSync.stream())
 }
 
 gulp.task('html', htmlTask)
