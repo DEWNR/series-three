36008b864edc6e49f839f3294710f434a5cfd832
Ben Kannenberg <bkannenberg@gmail.com>
2016-08-08 10:17:23 +0930

    Fix to only camel case keys

--- gulpfile.js/tasks/htmlData.js
+++ gulpfile.js/tasks/htmlData.js
@@ -2,16 +2,19 @@ var config       = require('../config')
 var gulp         = require('gulp')
 var jsoncombine  = require("gulp-jsoncombine")
 var path         = require('path')
+var _            = require('lodash')
 
 var htmlDataTask = function(cb) {
 
     return gulp.src(config.tasks.html.dataFiles)
 
         .pipe(jsoncombine("global.json",function(data){
-            var sData = JSON.stringify(data)
-            var sDataCC = sData.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase(); })
 
-            return new Buffer(sDataCC)
+            dataCC = _.mapKeys(data, function (v, k) {return _.camelCase(k);})
+
+            var sData = JSON.stringify(dataCC)
+
+            return new Buffer(sData)
         }))
         .pipe(gulp.dest("./src/html/data"));
 
